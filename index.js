b6ab68478d4d7d74f03e29d6938bdd19026fbd0e
vivacious-queue-3174 <caryaharper@gmail.com>
2020-05-15 02:42:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-var animal = 'dog';
+var animal = 'dog'
 
 function myAnimal() {
-  return animal;
-}
-
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  var animal = 'cat'
+  // You should not need to modify this function
   return animal
 }
 
-function add2(n) { 
-  // Feel free to move things around!
-  const two = 2
-  return n + two
+function yourAnimal() {
+  // The tests expect this function to return `animal` just like the previous function
+  // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: Hard-coding 'cat' below will not work
+  return animal
 }
 
-var funkyFunction = function funkyFunction() {
-  return function theFunk() {
+function add2(n) {
+  return n + two
+
+  // Feel free to move things around!
+  const two = 2
+}
+
+var funkyFunction = function() {
+  return function() {
     return "FUNKY!"
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()()
+var theFunk = funkyFunction
